584f7a17bc1c5ae17dfef47c6387fd91331d705f
Anastasya Yakimec <youremail@gmail.com>
2022-06-07 14:34:32 +0300

    add modal.Sign in

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,20 @@
-let user = null;
-let userSignIn = false;
-let sort = "New First";
-let card = null;
-let cartDataTime = null;
-let clients = null;
-let cartUser = null;
+// let user = null;
+// let userSignIn = false;
+// let sort = "New First";
+// let card = null;
+// let cartDataTime = null;
+// let clients = null;
+// let cartUser = null;
 
 
+let modalSignIn = document.getElementById("modalSignIn");
+let close = document.getElementById("close");
+let openSignIn = document.getElementById("openSignIn");
 
-
-
+close.onclick = () => {
+  modalSignIn.style.display = "none";
+};
+openSignIn.onclick = () => {
+  modalSignIn.style.display = "block";
+};
 
