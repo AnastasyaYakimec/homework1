c90cc5e2680a52832739157162a3beb558d9c837
Anastasya Yakimec <youremail@gmail.com>
2022-06-07 16:25:12 +0300

    add header

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,6 @@
 // let clients = null;
 // let cartUser = null;
 
-
 let modalSignIn = document.getElementById("modalSignIn");
 let close = document.getElementById("close");
 let openSignIn = document.getElementById("openSignIn");
@@ -18,3 +17,9 @@ openSignIn.onclick = () => {
   modalSignIn.style.display = "block";
 };
 
+let userMenu = document.getElementById("userMenu");
+let avatar = document.getElementById("avatar");
+avatar.onclick = () => {
+  // userMenu.style.display = "block";
+    
+};
