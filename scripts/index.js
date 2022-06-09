ceccacc3cfa8d88752b4070ed378e040031a3a62
Anastasya Yakimec <youremail@gmail.com>
2022-06-09 21:10:08 +0300

    add itempage

--- scripts/index.js
+++ scripts/index.js
@@ -11,10 +11,24 @@ let closeSignIn = document.getElementById("closeSignIn");
 let openSignIn = document.getElementById("openSignIn");
 
 closeSignIn.onclick = () => {
-  console.log("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
   modalSignIn.style.display = "none";
 };
 openSignIn.onclick = () => {
-  console.log("ssssssssssssssssssssssssssssssssssssssssss");
   modalSignIn.style.display = "block";
 };
+
+const openItemPage = document.getElementById("item-page");
+const closeItemPage = document.getElementById("closeItemPage");
+const qweerty = document.getElementsByClassName("list-variant-li");
+for (let i = 0; i < qweerty.length; i++) {
+  qweerty[i].onclick = () => {
+    console.log("ksdduisdudsldiiu");
+    openItemPage.style.display = "block";
+  };
+
+  closeItemPage.onclick = () => {
+    openItemPage.style.display = "none";
+  };
+
+  
+}
