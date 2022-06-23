8a00a948056fd469d009ee9d75ed23c839169ce6
AnastasyaYakimec <manuyna1978@gmail.com>
2022-06-23 18:51:13 +0300

    add generateDynamicClients

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let user = null;
 let userSignIn = false;
 let sort = "New First";
-let card = [
+const cards = [
   {
     id: "0",
     cardImg: "img/foto man v vr.svg",
@@ -49,7 +49,35 @@ let card = [
 ];
 let cartDataTime = null;
 let clients = null;
-let cartUser = null;
+const cartUser = [
+  {
+    title: "starstar ",
+    deckription:
+      "I know in real-time where the money is spent,and i don't have to land out the company's credit card anymore.What is relirf!",
+    avatar: "img/avatar1.svg",
+    imageAlt: "avatar girl",
+    name: "Denny Hilguston",
+    email: "@denny.hill",
+  },
+  {
+    title: "starstar ",
+    deckription:
+      "VISUALS are much better. The improvements in optics and resolution and much more than  modern consolea generation leap.",
+    avatar: "img/avatar2.svg",
+    imageAlt: "avatar girl",
+    name: "Vani Pandey",
+    email: "@vani.pandey",
+  },
+  {
+    title: "starstar ",
+    deckription:
+      "I know in real-time where the money is spent,and i don't have to land out the company's credit card anymore.What is relirf!",
+    avatar: "img/avatar2.svg",
+    imageAlt: "avatar girl",
+    name: "Vani Pandey",
+    email: "@vani.pandey",
+  },
+];
 
 let modalSignIn = document.getElementById("modalSignIn");
 let closeSignIn = document.getElementById("closeSignIn");
@@ -62,11 +90,10 @@ openSignIn.onclick = () => {
   modalSignIn.style.display = "block";
 };
 
-const qweerty = document.getElementsByClassName("list-variant-li");
 const generateDynamicCards = () => {
   const listShopVariant = document.getElementById("list-shop-variant");
-  card.forEach((article) => {
-    const articleHtml = ` <li class="list-variant-li">${article.id}
+  cards.forEach((article) => {
+    const articleHtml = ` <li class="list-variant-li" id="${article.id}">
               <div class="grey-b">
                 <h4 class="card-subtaitle">${article.cardName}</h4>
                 <p class="card-text">${article.cardText} </p>
@@ -80,38 +107,62 @@ const generateDynamicCards = () => {
     listShopVariant.appendChild(articleDiv);
   });
 };
-generateDynamicCards();
 
+generateDynamicCards();
+const mainItemPage = document.getElementById("main-item-page");
 const generateItemPage = (id) => {
-  const mainItemPage = document.getElementById("main-item-page");
-  const article = card.find((item) => item.id == id);
+  const article = cards.find((item) => item.id == id);
   const mainItemPageHtml = `
-      <span id ='closeItemPage' class="close-item-page">&times;</span>
+  <button id="closeItemPage"  class="mainBtn" onclick="closeDialog()">
+  <span class="close-item-page">&times;</span>
+  </button>
       <img class="main-img" src="./assets/img/foto man v vr.svg" alt="">
-      
       <div class="main-cart">
         <h4 class="header-title">${article.cardName}</h4>
         <p class="header-text">${article.cardText}</p>
         <button class="primary-btn">Get it Now</button>
       </div>
+       
     `;
   const articleDiv = document.createElement("div");
-  articleDiv.style.display = "flex";
   articleDiv.innerHTML = mainItemPageHtml;
   mainItemPage.appendChild(articleDiv);
 };
-
+const homePageCards = document.getElementsByClassName("list-variant-li");
 const openItemPage = document.getElementById("item-page");
-for (let i = 0; i < qweerty.length; i++) {
-  qweerty[i].onclick = () => {
-    openItemPage.style.display = "block";
-    generateItemPage(i);
+
+for (let i = 0; i < homePageCards.length; i++) {
+  homePageCards[i].onclick = () => {
+    openItemPage.style.display = "flex";
+    generateItemPage(homePageCards[i].id);
   };
 }
-const closeItemPage = document.getElementsByClassName("primary-btn");
-console.log(closeItemPage);
-console.log(openItemPage.style);
-closeItemPage.onclick = () => {
-  console.log("sdhfksdjkhf");
+const closeDialog = () => {
+  mainItemPage.innerHTML = null;
   openItemPage.style.display = "none";
 };
+
+const generateDynamicClients = () => {
+  const listCartUser = document.getElementById("listCartUser");
+  console.log(listCartUser);
+  cartUser.forEach((article) => {
+    const articleHtml = `<li class="cart-user-1">
+              <span class="cart-title">${article.title}<span> star</span></span>
+              <p class="cart-p">${article.deckription}
+               </p>
+              <div class="user-info">
+                // <img class="avatar-girl"${article.avatar} src="./assets/img/avatar1.svg" ${article.imageAlt}alt="avatar girl">
+                <div class="cart-footer-div">
+                  <span class="cart-footer-span">${article.name}</span>
+                  <span class="cart-footer-span-email">${article.email}</span>
+                </div>
+              </div>
+            </li>`;
+
+    const articleDiv = document.createElement("div");
+    articleDiv.style.display = "flex";
+    articleDiv.innerHTML = articleHtml;
+    listCartUser.appendChild(articleDiv);
+  });
+};
+generateDynamicClients();
